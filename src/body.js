7628c1037e28debe992f787a4b3303dc1f55459f
Marcus <marcus.kittelson@ucalgary.ca>
2023-03-08 12:05:32 -0700

    Broken Clock But Everything Else works

--- src/body.js
+++ src/body.js
@@ -1,24 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
 const Main = ({ activeNote, onUpdateNote, onDeleteNote }) => {
   const [editMode, setEditMode] = useState(true);
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [lastModified, setLastModified] = useState(Date.now());
+
+  useEffect(() => {
+    setTitle((activeNote || {}).title || '');
+    setBody((activeNote || {}).body || '');
+    setLastModified((activeNote || {}).lastModified || Date.now());
+  }, [activeNote]);
 
   const onEditField = (field, value) => {
-    onUpdateNote({
-      ...activeNote,
-      [field]: value,
-      lastModified: Date.now(),
-    });
+    if (editMode) {
+      if (field === 'title') {
+        setTitle(value);
+      } else if (field === 'body') {
+        setBody(value);
+      }
+    }
   };
 
   const handleSaveClick = () => {
+    onUpdateNote({
+      ...(activeNote || {}),
+      title,
+      body,
+      lastModified,
+    });
     setEditMode(false);
   };
 
   const handleEditClick = () => {
     setEditMode(true);
+  };
+
+  const handleCalendarChange = (e) => {
+    const date = new Date(e.target.value);
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const day = date.getDate();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    setLastModified(new Date(year, month, day, hour, minute).toISOString());
   };
 
   if (!activeNote) return <div className="no-active-note">No Active Note</div>;
@@ -36,18 +63,30 @@ const Main = ({ activeNote, onUpdateNote, onDeleteNote }) => {
           type="text"
           id="title"
           placeholder="Note Title"
-          value={activeNote.title}
+          value={title}
           onChange={(e) => onEditField('title', e.target.value)}
           autoFocus
+          disabled={!editMode}
         />
-        <div className= "note-meta">
-          <span class = "time">
-            Last Modified:{' '}
-            {new Date(activeNote.lastModified).toLocaleDateString('en-GB', {
-              hour: '2-digit',
-              minute: '2-digit',
-            })}
-          </span>
+        <div className="note-meta">
+          {editMode ? (
+            <span class="time">
+              <input
+                type="datetime-local"
+                value={new Date(lastModified).toISOString().slice(0, -8)}
+                onChange={handleCalendarChange}
+                disabled={!editMode}
+              />
+            </span>
+          ) : (
+            <span class="time">
+              Last Modified:{' '}
+              {new Date(lastModified).toLocaleDateString('en-GB', {
+                hour: '2-digit',
+                minute: '2-digit',
+              })}
+            </span>
+          )}
         </div>
         <div className="note-buttons">
           {editMode ? (
@@ -61,15 +100,12 @@ const Main = ({ activeNote, onUpdateNote, onDeleteNote }) => {
         </div>
       </div>
       {editMode ? (
-        <div className="app-main-note-edit">
-          <ReactQuill
-            value={activeNote.body}
-            onChange={(value) => onEditField('body', value)}
-          />
+        <div className="note-editor">
+          <ReactQuill value={body} onChange={(value) => onEditField('body', value)} />
         </div>
       ) : (
-        <div className="app-main-note-display">
-          <div className="ql-editor" dangerouslySetInnerHTML={{ __html: activeNote.body }}></div>
+        <div className="note-viewer">
+          <ReactQuill value={body} readOnly />
         </div>
       )}
     </div>
